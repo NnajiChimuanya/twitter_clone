bc642d8f7f09a70182785b28555372201a31bfc2
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-05-30 23:27:12 +0400

    Set the max-width to 1300px

--- src/feed/Feed.js
+++ src/feed/Feed.js
@@ -4,7 +4,14 @@ import React from 'react'
 const Feed = () => {
   return (
    <div className="feed">
-       Im the feed
+      {/* Header */}
+      <div className="feed-header">
+        <h2> Home </h2>
+      </div>
+
+      {/* tweet box */}
+
+      {/* posts */}
    </div>
   )
 }
