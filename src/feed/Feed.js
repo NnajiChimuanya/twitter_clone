a41771c332ea02eac79eda7b587d6258289c3010
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-06-06 10:06:11 +0400

    Initialized firebase and created the db

--- src/feed/Feed.js
+++ src/feed/Feed.js
@@ -1,9 +1,16 @@
 import "./feed.css"
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import TweetBox from "./TweetBox"
 import Post from "./Post"
+import db from "../firebase"
 
 const Feed = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+
+  }, [])
+
   return (
    <div className="feed">
       {/* Header */}
@@ -13,7 +20,14 @@ const Feed = () => {
 
       < TweetBox />
 
-      < Post />
+      < Post 
+      displayName="Nnaji Chimuanya"
+      userName="nnaji_chimuanya"
+      verified
+      text="This is the first post"
+      avatar="https://pbs.twimg.com/profile_images/1521238952698601475/qIg6IES6_400x400.jpg"
+      image="https://i.gifer.com/QXLH.gif"
+      />
      
       
 
