5d06fd218c7b147c7d15792b99c1fa261e7740f7
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-06-06 07:52:11 +0400

    Added the react-twitter-embed which is for the widget

--- src/widget/Widget.js
+++ src/widget/Widget.js
@@ -1,11 +1,31 @@
 import "./widget.css"
 import React from 'react'
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 const Widgets = () => {
   return (
    <div className="widgets">
-       <h3> I am the widgets</h3>
-   </div>
+      <div className="widget-input">
+        <SearchOutlinedIcon className="widget-input-search" />
+        <input type="text" placeholder="Search" />
+      </div>
+
+      <div className="widget-container">
+        <h2> What's happening</h2>
+
+        <TwitterTimelineEmbed 
+          sourceType="profile" 
+          screenName="nnaji_chimuanya" 
+          options={{height : 400}} 
+        />
+
+        <TwitterTweetEmbed
+          tweetId={'1410465812452945926'}
+        />
+      </div>
+
+    </div>
   )
 }
 
