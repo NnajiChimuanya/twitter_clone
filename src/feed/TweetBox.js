3ae4e1ddb48083f33e83a6760a84968c0d1e151b
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-06-07 05:17:46 +0400

    created the useState for tweet and image

--- src/feed/TweetBox.js
+++ src/feed/TweetBox.js
@@ -1,20 +1,36 @@
 import "./tweetBox.css"
-import React from 'react'
+import React, { useState } from 'react'
 import { Avatar, Button } from "@material-ui/core"
+import { database } from "../firebase"
 
 const TweetBox = () => {
+
+  const [tweet, setTweet] = useState("")
+  const [image, setImage] = useState("")
+
+
+  const sendTweet = (e) => {
+    e.preventDefault()
+
+
+
+    setTweet("")
+    setImage("")
+  }
+
   return (
    <div className="tweetBox">
         <form>
             <div className="tweetBox-input">
                 < Avatar src="https://pbs.twimg.com/profile_images/1521238952698601475/qIg6IES6_400x400.jpg" />
-                < input type="text" placeholder="What's happening?" />
+                < input value={tweet} onChange={e => setTweet(e.target.value)} type="text" placeholder="What's happening?" />
                 {/* < input type="file" /> */}
             </div>
 
-            <input type="text" placeholder="Optional image url" />
+            <input value={image} onChange={e => setImage(e.target.value)} type="text" placeholder="Optional image url" />
 
-            <Button className="tweetBox-tweet"> Tweet </Button>
+            <input type="file" name="myImage" accept="image/png, image/gif, image/jpeg" />
+            <Button onClick={sendTweet} className="tweetBox-tweet"> Tweet </Button>
         </form>
    </div>
   )
