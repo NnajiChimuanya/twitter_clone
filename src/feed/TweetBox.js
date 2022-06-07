6c81f9cb0c15a9f008bfa4b746d1e7768682a313
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-06-07 06:03:10 +0400

    Changed the default choose file to an image collection icon, and made it to accep only images and gifs

--- src/feed/TweetBox.js
+++ src/feed/TweetBox.js
@@ -2,8 +2,11 @@ import "./tweetBox.css"
 import React, { useState } from 'react'
 import { Avatar, Button } from "@material-ui/core"
 import { database } from "../firebase"
+import CollectionsIcon from '@mui/icons-material/Collections';
+import {collection } from "firebase/firestore";
 
 const TweetBox = () => {
+  const collectionRef = collection(database, "76149494ABMICTU")
 
   const [tweet, setTweet] = useState("")
   const [image, setImage] = useState("")
@@ -12,6 +15,9 @@ const TweetBox = () => {
   const sendTweet = (e) => {
     e.preventDefault()
 
+  
+
+    alert("Clicked")
 
 
     setTweet("")
@@ -25,11 +31,18 @@ const TweetBox = () => {
                 < Avatar src="https://pbs.twimg.com/profile_images/1521238952698601475/qIg6IES6_400x400.jpg" />
                 < input value={tweet} onChange={e => setTweet(e.target.value)} type="text" placeholder="What's happening?" />
                 {/* < input type="file" /> */}
+                <div className="image-upload">
+              <label for="image">
+                < CollectionsIcon />
+              </label>
+              <input id="image" type="file" name="myImage" accept="image/png, image/gif, image/jpeg" />
+            </div>
             </div>
 
             <input value={image} onChange={e => setImage(e.target.value)} type="text" placeholder="Optional image url" />
 
-            <input type="file" name="myImage" accept="image/png, image/gif, image/jpeg" />
+      
+
             <Button onClick={sendTweet} className="tweetBox-tweet"> Tweet </Button>
         </form>
    </div>
